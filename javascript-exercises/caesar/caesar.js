13ce47425b61685afed8dcfa5885a86b2a6ca8c1
mrjackmaster <jacektrg+github@gmail.com>
2021-04-16 17:42:20 +0200

    Small clarity change.

--- javascript-exercises/caesar/caesar.js
+++ javascript-exercises/caesar/caesar.js
@@ -1,33 +1,32 @@
 const alphabetArray = [...'abcdefghijklmnopqrstuvwxyz'];
-const caesar = function(string, number) {
+const caesar = function(oldString, number) {
 	let newString = '';
-	for (let i = 0; i < string.length; i++) {
-		oldLetter = string[i];
+	for (let i = 0; i < oldString.length; i++) {
+		oldLetter = oldString[i];
 		if (alphabetArray.indexOf(oldLetter.toLowerCase()) == -1) {
 			newString += oldLetter;
 		}
 		else if (oldLetter == oldLetter.toLowerCase()) {
-			newLetter = alphabetArray.indexOf(oldLetter) + number;
-			while (newLetter > 25) {
-				newLetter -= 26;
+			newLetterIndex = alphabetArray.indexOf(oldLetter) + number;
+			while (newLetterIndex > 25) {
+				newLetterIndex -= 26;
 			}
-			while (newLetter < 0) {
-				newLetter += 26;
+			while (newLetterIndex < 0) {
+				newLetterIndex += 26;
 			}
-			newString += alphabetArray[newLetter];
+			newString += alphabetArray[newLetterIndex];
 		}
 		else {
 			oldLetter = oldLetter.toLowerCase();
-			newLetter = alphabetArray.indexOf(oldLetter) + number;
-			while (newLetter > 25) {
-				newLetter -= 26;
+			newLetterIndex = alphabetArray.indexOf(oldLetter) + number;
+			while (newLetterIndex > 25) {
+				newLetterIndex -= 26;
 			}
-			while (newLetter < 0) {
-				newLetter += 26;
+			while (newLetterIndex < 0) {
+				newLetterIndex += 26;
 			}
-			newLetter = alphabetArray[newLetter];
-			newLetter = newLetter.toUpperCase();
-			newString += newLetter;
+			newLetter = alphabetArray[newLetterIndex];
+			newString += newLetter.toUpperCase();
 		}
 	}
 	return newString;
